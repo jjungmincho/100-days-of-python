7e967fd7a5a23d35cc8d8e1624ba5289278a7c71
Jessica Cho <jessicacho2013@gmail.com>
2021-08-04 20:41:06 -0700

    Create average height project for day 5

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Master Python by building 100 projects in 100 days
 - Day 2: Tip calculator 
 - Day 3: Treasure Islands
 - Day 4: Rock Paper Scissors 
+- Day 5: Average Height
+
 
 ### Source
 [100 Days of Code by Angela Yu](https://www.udemy.com/course/100-days-of-code/)
---
+++ day5/average_height.py
@@ -0,0 +1,24 @@
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+# using "sum" function
+# total_height = sum(student_heights)
+# implent by using "for"
+total_height = 0
+for height in student_heights:
+  total_height += height
+#print(total_height)
+
+# using "len" function
+# number_of_students = len(student_heights)
+# implent by using "for"
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+#print(number_of_students)
+
+average_height = round(total_height / number_of_students)
+print(average_height)
+
+
